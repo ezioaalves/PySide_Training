1bae8fbdd636bdd76a307c03a7c646db75900f74
ezio.a <ezio.a@escolar.ifrn.edu.br>
2022-05-19 12:27:42 -0300

    first class

---
+++ main.py
@@ -0,0 +1,28 @@
+#IMPORT MODULES
+import sys
+import os
+
+#IMPORT QT CORE
+from qt_core import *
+
+#IMPORT MAIN WINDOW
+from gui.windows.main_win.ui_main_window import *
+
+#MAIN WINDOW
+class MainWindow(QMainWindow):
+  def __init__(self):
+      super().__init__()
+
+      #SETUP MAIN WINDOW
+      self.ui = UI_MainWindow()
+      self.ui.setup_ui(self)
+
+
+      #EXIBE A NOSSA APLICAÇÃO
+      self.show()
+
+if __name__ == "__main__":
+  app = QApplication(sys.argv)
+  window = MainWindow()
+  sys.exit(app.exec())
+  
---
+++ qt_core.py
@@ -0,0 +1,6 @@
+#IMPORT MODULES
+
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+from PySide6.QtSvgWidgets import *
