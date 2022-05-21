fa9b4447b2d37010076f8c0dbc0fb96d724db153
ezio.a <ezio.a@escolar.ifrn.edu.br>
2022-05-21 00:07:56 -0300

    Fourth Class

--- main.py
+++ main.py
@@ -19,9 +19,29 @@ class MainWindow(QMainWindow):
       self.ui = UI_MainWindow()
       self.ui.setup_ui(self)
 
+      # TOGGLE BUTTON
+      self.ui.toggle_button.clicked.connect(self.toggle_button)
 
       #EXIBE A NOSSA APLICAÇÃO
       self.show()
+  
+  def toggle_button(self):
+    # GET MENU WIDTH
+    menu_width = self.ui.left_menu.width()
+
+    # CHECK WITH
+    width = 50
+    if menu_width == 50:
+      width = 240
+    
+    # START ANIMATION
+    self.animation = QPropertyAnimation(self.ui.left_menu, b"minimumWidth")
+    self.animation.setStartValue(menu_width)
+    self.animation.setEndValue(width)
+    self.animation.setDuration(500)
+    self.animation.setEasingCurve(QEasingCurve.InOutCirc)
+    self.animation.start()
+
 
 if __name__ == "__main__":
   app = QApplication(sys.argv)
